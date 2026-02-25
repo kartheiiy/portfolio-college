a3f337328dcac7f09ba53196975ffa6922eabc35
Karthik chandra <karthikchandra23@gmail.com>
2026-02-25 22:03:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+  const typed = new Typed('.multiple-text', {
+      strings: ['MCA Student', 'Web Designer', 'Backend Developer'],
+      typeSpeed: 80,
+      backspeed:80,
+      backdelay:1200,
+      loop:true,
+  });
