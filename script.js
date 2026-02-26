e5e65c57d5d7ec7f47719cc7d0e7af79dfd29481
Karthik chandra <karthikchandra23@gmail.com>
2026-02-26 21:30:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-  const typed = new Typed('.multiple-text', {
-      strings: ['MCA Student', 'Web Designer', 'Backend Developer'],
-      typeSpeed: 80,
-      backspeed:80,
-      backdelay:1200,
-      loop:true,
-  });
